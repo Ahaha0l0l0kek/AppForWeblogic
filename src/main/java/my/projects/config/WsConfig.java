3be3066c10441f8b4e15cb0c3401f6342df80483
Axelerator <lexabuchkov@gmail.com>
2021-11-24 22:45:01 +0300

    add security

--- src/main/java/my/projects/config/WsConfig.java
+++ src/main/java/my/projects/config/WsConfig.java
@@ -1,5 +1,6 @@
 package my.projects.config;
 
+import org.apache.wss4j.dom.WSConstants;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -8,10 +9,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.server.EndpointInterceptor;
+import org.springframework.ws.server.endpoint.interceptor.PayloadLoggingInterceptor;
+import org.springframework.ws.soap.security.wss4j2.Wss4jSecurityInterceptor;
+import org.springframework.ws.soap.security.wss4j2.callback.SimplePasswordValidationCallbackHandler;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
+
+import java.util.Collections;
+import java.util.List;
 
 @EnableWs
 @Configuration
@@ -38,5 +46,31 @@ public class WsConfig extends WsConfigurerAdapter {
     @Bean
     public XsdSchema productSchema() {
         return new SimpleXsdSchema(new ClassPathResource("product.xsd"));
+    }
+
+    @Bean
+    SimplePasswordValidationCallbackHandler callbackHandler() {
+        SimplePasswordValidationCallbackHandler callbackHandler = new SimplePasswordValidationCallbackHandler();
+        callbackHandler.setUsersMap(Collections.singletonMap("userName", "password"));
+        return callbackHandler;
+    }
+
+    @Bean
+    public Wss4jSecurityInterceptor wss4jSecurityInterceptor() {
+        Wss4jSecurityInterceptor interceptor = new Wss4jSecurityInterceptor();
+        interceptor.setValidationActions(WSConstants.USERNAME_TOKEN_LN);
+        interceptor.setValidationCallbackHandler(callbackHandler());
+        return interceptor;
+    }
+
+    @Bean
+    PayloadLoggingInterceptor payloadLoggingInterceptor() {
+        return new PayloadLoggingInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(List<EndpointInterceptor> interceptors) {
+        interceptors.add(payloadLoggingInterceptor());
+        interceptors.add(wss4jSecurityInterceptor());
     }
 }
